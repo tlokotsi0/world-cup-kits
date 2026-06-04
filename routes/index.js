d6c23d14dedad509a5a35f7e46ce7f0e64fa9e56
lcarlo23 <160224871+lcarlo23@users.noreply.github.com>
2026-06-04 17:21:54 +0200

    Create main route with swagger and authentication routes

---
+++ routes/index.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import productsRoute from './products.js';
+import usersRoute from './users.js';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json' with { type: 'json' };
+import passport from 'passport';
+
+const router = express.Router();
+
+const swaggerOptions = {
+  customCss: '.auth-wrapper { display: none !important; }',
+};
+
+router.use('/events', productsRoute);
+router.use('/locations', usersRoute);
+router.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, swaggerOptions),
+);
+
+router.get(
+  '/login',
+  passport.authenticate('github', () => {}),
+);
+router.get('/logout', (req, res, next) => {
+  req.logout((err) => {
+    if (err) return next(err);
+    res.redirect('/');
+  });
+});
+
+router.get('/', (req, res) => {
+  res.send(
+    req.session.user !== undefined
+      ? `Logged in as ${req.session.user.displayName}`
+      : 'Logged Out',
+  );
+});
+
+router.get(
+  '/github/callback',
+  passport.authenticate('github', {
+    failureRedirect: '/api-docs',
+    session: false,
+  }),
+  (req, res) => {
+    req.session.user = req.user;
+    res.redirect('/');
+  },
+);
+
+export default router;
